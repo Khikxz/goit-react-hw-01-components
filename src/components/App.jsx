f711e587287a8234184b72e5fa19f8c1242e015d
Khikxz <140308359+Khikxz@users.noreply.github.com>
2024-03-04 05:50:09 +0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import User from "./User/User.jsx"
+import Data from "./Data/Data.jsx"
+import Friends from "./Friends/Friends.jsx"
+import Transaction from "./Transactions/Transaction.jsx";
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      REACT HOMEWORK-1: COMPONENTS
+      <User/>
+      <Data/>
+      <Friends/>
+      <Transaction/>
     </div>
   );
 };
---
+++ src/components/Friends/Friends.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import friends from './friends.json';
+
+const Friends = () => {
+    return <li class="item">
+    <span class="status"></span>
+    <img class="avatar" src="" alt="User avatar" width="48" />
+    <p class="name"></p>
+  </li>
+};
+
+export default Friends;
